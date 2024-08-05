96f3e8c17b567849e5a129f2b7c1f7a5beff14e6
kngssd <danakengessova@gmail.com>
2024-08-05 16:35:48 +0100

    Task 1  -solution (Dana)

--- src/org/academy/langton/Ant.java
+++ src/org/academy/langton/Ant.java
@@ -34,18 +34,16 @@ public class Ant {
     }
 
     public void update() {
-        //TODO: your task.  Make the ant move according to the rules in:
-        // https://codingdojo.org/kata/LangtonAnt/
-        // https://en.wikipedia.org/wiki/Langton%27s_ant
-        // Specifically the “ant” moves according to the rules below:
-        // At a white square, turn 90° right, flip the color of the square, move forward one unit
-        // At a black square, turn 90° left, flip the color of the square, move forward one unit
+        Cell thisCell = ground.cellAt(gridPosition);
 
+        if (thisCell.isActive()) {
+            turnClockwise();
+        } else {
+            turnCounterclockwise();
+        }
+        thisCell.toggleActive();
         moveForward();
-
         //Notes: In the ground object, "white" squares are "inactive", "black" squares are "active"
-
-
     }
 
     private void moveForward() {
